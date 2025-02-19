1894e93f6abd69dad80dd096cd66152708a6ad65
Duckcyzzz <ducbanca1604@gmail.com>
2025-02-19 16:12:54 +0700

    fix(UI): change Board assets

--- Gop_repo/chesslogic/Seahorse_Chess_Game-fba2f03cefd29cd239256670eba5469301e5f844/src/main/IsometricBoard.java
+++ Gop_repo/chesslogic/Seahorse_Chess_Game-fba2f03cefd29cd239256670eba5469301e5f844/src/main/IsometricBoard.java
@@ -21,20 +21,38 @@ public class IsometricBoard extends JPanel{
     private int lastMouseX, lastMouseY;
 
     private BufferedImage tileImage;
-    private BufferedImage test1;
-    private BufferedImage test2;
-    private BufferedImage test3;
-    private BufferedImage test4;
+    private BufferedImage tileImage2;
+    private BufferedImage blueflat;
+    private BufferedImage blue;
+    private BufferedImage greenflat;
+    private BufferedImage green;
+    private BufferedImage redflat;
+    private BufferedImage red;
+    private BufferedImage yellowflat;
+    private BufferedImage yellow;
+    private BufferedImage bluecircle;
+    private BufferedImage greencircle;
+    private BufferedImage redcircle;
+    private BufferedImage yellowcircle;
+
     public IsometricBoard(){
         try {
-            tileImage = ImageIO.read(new File("./assets/Isometric_Tiles_Pixel_Art/Blocks/blocks_12.png"));
-            test1 = ImageIO.read(new File("./assets/Isometric_Tiles_Pixel_Art/Blocks/blocks_30.png"));
-            test2 = ImageIO.read(new File("./assets/Isometric_Tiles_Pixel_Art/Blocks/blocks_31.png"));
-            test3 = ImageIO.read(new File("./assets/Isometric_Tiles_Pixel_Art/Blocks/blocks_35.png"));
-            test4 = ImageIO.read(new File("./assets/Isometric_Tiles_Pixel_Art/Blocks/blocks_36.png"));
+            tileImage = ImageIO.read(new File("./assets/Land/blocks_white.png"));
+            tileImage2 = ImageIO.read(new File("./assets/Land/blocks_white_flat.png"));
+            blueflat = ImageIO.read(new File("./assets/Land/blocks_blue_flat.png"));
+            blue = ImageIO.read(new File("./assets/Land/blocks_blue.png"));
+            greenflat = ImageIO.read(new File("./assets/Land/blocks_green_flat.png"));
+            green = ImageIO.read(new File("./assets/Land/blocks_green.png"));
+            redflat = ImageIO.read(new File("./assets/Land/blocks_red_flat.png"));
+            red = ImageIO.read(new File("./assets/Land/blocks_red.png"));
+            yellowflat = ImageIO.read(new File("./assets/Land/blocks_yellow_flat.png"));
+            yellow = ImageIO.read(new File("./assets/Land/blocks_yellow.png"));
+            bluecircle = ImageIO.read(new File("./assets/Land/blocks_blue_circle_flat.png"));
+            greencircle = ImageIO.read(new File("./assets/Land/blocks_green_circle_flat.png"));
+            redcircle = ImageIO.read(new File("./assets/Land/blocks_red_circle_flat.png"));
+            yellowcircle = ImageIO.read(new File("./assets/Land/blocks_yellow_circle_flat.png"));
             
         } catch (IOException e) {
-            e.printStackTrace();
         }
         setBackground(Color.BLACK);
         addMouseListener(new MouseAdapter() {
@@ -67,32 +85,60 @@ public class IsometricBoard extends JPanel{
                 int x = startX + (col - row) * TILE_WIDTH/2 ;
                 int y = startY + (col + row) * TILE_HEIGHT/2 ;
                 BufferedImage currentImage;
-                if(row < 6 && col < 6){
-                    currentImage = test1;
+                if(row==7 && col == 0){
+                    currentImage = greencircle;
+                }
+                else if(row==0 && col == 7){
+                    currentImage = bluecircle;
+                }
+                else if(row==7 && col == 14){
+                    currentImage = redcircle;
+                }
+                else if(row==14 && col == 7){
+                    currentImage = yellowcircle;
+                }
+                else if((row==1&&col==1)|| (row==1)&&(col==4)|| (row==4&&col==1)||(row==4&&col==4)){
+                    currentImage = tileImage2;
+                }
+                else if((col == 7 && row >= 1 && row <= 6)||(row == 0 && col == 6)){
+                    currentImage = blue;
+                }
+                else if ((row==10&&col==1)||(row==10&&col==4)||(row==13&&col==1)||(row==13&&col==4)) {
+                    currentImage = tileImage2;
+                }
+                else if((col >= 1 && col <= 6 && row == 7)|| (row == 8 && col == 0)){
+                    currentImage = green;
+                }
+                else if ((row==1&&col==10)||(row==1&&col==13)||(row==4&&col==10)||(row==4&&col==13)) {
+                    currentImage = tileImage2;
+                }
+                else if((row == 7 && col >= 8 && col < 14)|| (row == 6 && col == 14)){
+                    currentImage = red;
+                }
+                else if ((row==10&&col==10)||(row==10&&col==13)||(row==13&&col==10)||(row==13&&col==13)) {
+                    currentImage = tileImage2;
+                }
+                else if((col == 7 && row >= 8 && row < 14) ||(row == 14 && col == 8)){
+                    currentImage = yellow;
+                }
+                else if(row < 6 && col < 6){
+                    currentImage = blueflat;
                 }else if(row >= 9 && col < 6){
-                    currentImage = test2;
+                    currentImage = greenflat;
                 }else if(row < 6 && col >= 9){
-                    currentImage = test3;
+                    currentImage = redflat;
                 }else if(row >= 9 && col >= 9){
-                    currentImage = test4;
-                }else if(col >= 1 && col <= 6 && row == 7){
-                    currentImage = test2;
+                    currentImage = yellowflat;
                 }
-                else if(col == 7 && row >= 8 && row < 14){
-                    currentImage = test4;
-                }
-                else if(col == 7 && row >= 1 && row <= 6){
-                    currentImage = test1;
-                }
-                else if(row == 7 && col >= 8 && col < 14){
-                    currentImage = test3;
-
-                }else{
+                else{
                     currentImage = tileImage;
                 }
                 g2d.drawImage(currentImage, x, y, null);
             }
         }
+        // public void Paint(Garphics g){
+        
+        // }
         
     }
 
